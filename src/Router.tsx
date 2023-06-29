727f1d5e382fc464e4908b365b1edc2745f0c913
jioneee <qoxx3659@gmail.com>
2023-06-30 01:49:57 +0900

    feat: privat, public router 생성 및 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,9 @@
 import { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
+import { TOKEN_KEY } from './constants/auth';
 import { PATH } from './constants/path';
+import { getToken } from './utils/token';
 
 const SigninPage = lazy(() => import('@/pages/signin'));
 const SignupPage = lazy(() => import('@/pages/signup'));
@@ -11,12 +13,28 @@ function Router() {
   return (
     <Suspense>
       <Routes>
-        <Route path={PATH.TODO} element={<TodoPage />} />
-        <Route path={PATH.SIGNIN} element={<SigninPage />} />
-        <Route path={PATH.SIGNUP} element={<SignupPage />} />
+        <Route element={<PrivateRouter />}>
+          <Route path={PATH.TODO} element={<TodoPage />} />
+        </Route>
+        <Route element={<PublicRouter />}>
+          <Route path={PATH.SIGNIN} element={<SigninPage />} />
+          <Route path={PATH.SIGNUP} element={<SignupPage />} />
+        </Route>
       </Routes>
     </Suspense>
   );
 }
 
 export default Router;
+
+const PrivateRouter = () => {
+  const token = getToken(TOKEN_KEY);
+
+  return token ? <Outlet /> : <Navigate to={PATH.SIGNIN} replace />;
+};
+
+const PublicRouter = () => {
+  const token = getToken(TOKEN_KEY);
+
+  return token ? <Navigate to={PATH.TODO} replace /> : <Outlet />;
+};
