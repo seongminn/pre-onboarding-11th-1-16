f1e294a7115a4f05404cbcf5ec03cb8af4451706
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-06-30 01:34:30 +0900

    refactor: useInput 커스텀 훅 생성 및 로그인 페이지 적용

---
+++ src/constants/validation.ts
@@ -0,0 +1,4 @@
+export const VALIDATION = {
+  AT: '@',
+  MIN_LENGTH: 8,
+};
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,74 +1,39 @@
-import React, { ChangeEvent, FormEvent, useCallback, useEffect, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { postSignin } from '@/apis/auth';
+import useInput from '@/hooks/useInput';
 
 const Signin = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [signInError, setSignInError] = useState('');
-  const [validationError, setValidationError] = useState('');
+  const { value: credentials, onChange } = useInput({
+    email: '',
+    password: '',
+  });
   const [disabled, setDisabled] = useState(false);
   const navigate = useNavigate();
 
-  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSignIn = useCallback(
-    async (e: FormEvent) => {
-      e.preventDefault();
-
-      if (!email.includes('@')) {
-        setValidationError('유효한 이메일 주소를 입력해주세요.');
-        setDisabled(true);
-
-        return;
-      }
-
-      if (password.length < 8) {
-        setValidationError('비밀번호는 최소 8자 이상이어야 합니다.');
-        setDisabled(true);
-
-        return;
-      }
-
-      try {
-        const response = await postSignin({ email, password });
-
-        // localstorage 저장은 이미 postSignin 함수에서 처리했으므로 제거
-        navigate('/todo');
-        window.location.reload();
-      } catch (error: any) {
-        setSignInError(error.response.data);
-        console.log(error.response);
-      }
-    },
-    [email, password, navigate],
-  );
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
+  const handleSignIn = () => {
+    postSignin(credentials).then(() => {
       navigate('/todo');
-    }
-  }, [navigate]);
+    });
+  };
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    handleSignIn();
+  };
 
   return (
     <section>
-      <form onSubmit={handleSignIn}>
+      <form onSubmit={onSubmit}>
         <h1 className="title">로그인</h1>
         <label htmlFor="email">이메일</label>
         <input
           name="email"
           type="text"
-          value={email}
-          onChange={onChangeEmail}
+          value={credentials.email}
+          onChange={onChange}
           placeholder="이메일을 입력해주세요!"
           data-testid="email-input"
         />
@@ -76,16 +41,14 @@ const Signin = () => {
         <input
           name="password"
           type="password"
-          value={password}
-          onChange={onChangePassword}
+          value={credentials.password}
+          onChange={onChange}
           placeholder="비밀번호를 입력해주세요!"
           data-testid="password-input"
         />
         <button type="submit" disabled={disabled} data-testid="singin-button">
           제출
         </button>
-        {signInError && <div>{signInError}</div>}
-        {validationError && <div>{validationError}</div>}
       </form>
     </section>
   );
