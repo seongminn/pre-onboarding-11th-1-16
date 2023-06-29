0ca1b423b6b716138c93bf1b6193ca51fe9fc871
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-06-30 01:39:53 +0900

    refactor: useInput 커스텀 훅 회원가입 페이지에 적용

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,65 +1,32 @@
-import React, { ChangeEvent, FormEvent, useCallback, useEffect, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { postSignup } from '@/apis/auth';
+import useInput from '@/hooks/useInput';
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [signUpError, setSignUpError] = useState('');
-  const [validationError, setValidationError] = useState('');
+  const { value: credentials, onChange } = useInput({
+    email: '',
+    password: '',
+  });
   const [disabled, setDisabled] = useState(false);
   const navigate = useNavigate();
 
-  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+  const handleSignup = () => {
+    postSignup(credentials).then(() => {
+      navigate('/signin');
+    });
   };
 
-  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    handleSignup();
   };
-
-  const handleSignUp = useCallback(
-    async (e: FormEvent) => {
-      e.preventDefault();
-
-      if (!email.includes('@')) {
-        setValidationError('유효한 이메일 주소를 입력해주세요.');
-        setDisabled(true);
-
-        return;
-      }
-
-      if (password.length < 8) {
-        setValidationError('비밀번호는 최소 8자 이상이어야 합니다.');
-        setDisabled(true);
-
-        return;
-      }
-
-      try {
-        const response = await postSignup({ email, password });
-
-        navigate('/signin');
-      } catch (error: any) {
-        setSignUpError(error.response.data);
-        console.log(error.response);
-      }
-    },
-    [email, password, navigate],
-  );
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      navigate('/todo');
-    }
-  }, [navigate]);
 
   return (
     <section>
-      <form onSubmit={handleSignUp}>
+      <form onSubmit={onSubmit}>
         <h1 className="title">회원가입</h1>
         <label htmlFor="email" className="form-label">
           이메일
@@ -68,8 +35,8 @@ const Signup = () => {
           id="email"
           type="text"
           className="form-input"
-          value={email}
-          onChange={onChangeEmail}
+          value={credentials.email}
+          onChange={onChange}
           placeholder="이메일을 입력해주세요!"
           data-testid="email-input"
         />
@@ -80,16 +47,14 @@ const Signup = () => {
           id="password"
           type="password"
           className="form-input"
-          value={password}
-          onChange={onChangePassword}
+          value={credentials.password}
+          onChange={onChange}
           placeholder="비밀번호를 입력해주세요!"
           data-testid="password-input"
         />
         <button type="submit" disabled={disabled} data-testid="signup-button">
           제출
         </button>
-        {signUpError && <div>{signUpError}</div>}
-        {validationError && <div>{validationError}</div>}
       </form>
     </section>
   );
