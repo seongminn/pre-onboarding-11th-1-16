3932fcca5baf67854fe5407108373ac36d2b0861
jioneee <qoxx3659@gmail.com>
2023-06-30 13:00:36 +0900

    feat: Router 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
-
-import Signin from '@/pages/signin';
-import Signup from '@/pages/signup';
+import Router from '@/Router';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-      </Routes>
+      <Router />
     </div>
   );
 }
