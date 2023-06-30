d02016b69078f6db5eaad897cd7663400473a464
seongminn <berry9901@gmail.com>
2023-06-30 12:33:39 +0900

    refactor: 필요 없는 함수의 매개변수 제거 및 button 타입 지정

--- src/components/todo/TodoCreator/index.tsx
+++ src/components/todo/TodoCreator/index.tsx
@@ -8,17 +8,17 @@ const TodoCreator = () => {
   const { value, onChange, resetValue } = useInput({ todo: '' });
   const { dispatch } = useTodos();
 
-  const handleCreateTodo = (e: FormEvent<HTMLFormElement>, todo: string) => {
+  const handleCreateTodo = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    createTodo({ todo }).then(({ data }) => {
+    createTodo({ todo: value.todo }).then(({ data }) => {
       dispatch({ type: 'CREATE', todo: data });
       resetValue();
     });
   };
 
   return (
-    <form onSubmit={(e) => handleCreateTodo(e, value.todo)} className="create-form">
+    <form onSubmit={handleCreateTodo} className="create-form">
       <input
         name="todo"
         placeholder="새로운 투두를 입력해보세요!"
@@ -26,7 +26,9 @@ const TodoCreator = () => {
         onChange={onChange}
         data-testid="new-todo-input"
       />
-      <button data-testid="new-todo-add-button">추가</button>
+      <button type="submit" data-testid="new-todo-add-button">
+        추가
+      </button>
     </form>
   );
 };
