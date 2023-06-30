f0d48bba6ab5817c28e10fc35f736851bc36d39a
jsdmas <sdmas5730@gmail.com>
2023-06-30 19:40:30 +0900

    :art: Style : loader style 추가

--- src/components/common/Loader/index.tsx
+++ src/components/common/Loader/index.tsx
@@ -1,9 +1,42 @@
+import styled from '@emotion/styled';
+
 const Loader = () => {
   return (
-    <div className="spinner-wrapper">
-      <div className="spinner" />;
-    </div>
+    <LoaderWrapper className="spinner-wrapper">
+      <LoaderDiv className="spinner" />;
+    </LoaderWrapper>
   );
 };
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* 상하좌우 정중앙 정렬하기 */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const LoaderDiv = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 5px solid #fff;
+  border-bottom-color: rgb(65, 104, 177);
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default Loader;
